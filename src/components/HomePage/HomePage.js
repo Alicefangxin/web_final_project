919273ced017048bf4d31457d9134fe52393b8e7
Nicole Ni <shallwaytalk@gmail.com>
2022-11-21 19:49:45 -0800

    add in bootstrap/fortawesome dependencies (deleted useless files)

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,32 +1,47 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import "./home.css"
-
-
+import "./home.css";
 
 const HomePage = () => {
-    return (
-        <>
-            <div>
-                <h1>Northeastern University - rate my professor</h1>
-            </div>
-            <div className="wd-loginSignup wd-bgColor">
-                <div className="wd-lSContainer">
-                    <h4><button className="wd-round-pill">Log  In</button> OR <button className="wd-round-pill">Sign Up</button></h4>
-                    <input type="text" placeholder="username" id="username" className="wd-input"/>
-                    <input type="password" placeholder="password" id="password" className="wd-input"/>
-                </div>
-            </div>
+  return (
+    <>
+      <div>
+        <h1>Northeastern University - rate my professor</h1>
+      </div>
+      <div className="wd-loginSignup wd-bgColor">
+        <div className="wd-lSContainer">
+          <h4>
+            <button className="wd-round-pill">Log In</button> OR{" "}
+            <button className="wd-round-pill">Sign Up</button>
+          </h4>
+          <input
+            type="text"
+            placeholder="username"
+            id="username"
+            className="wd-input"
+          />
+          <input
+            type="password"
+            placeholder="password"
+            id="password"
+            className="wd-input"
+          />
+        </div>
+      </div>
 
-            <div className="wd-searchMain">
-                <i className="fas fa-cog fa-2x" style={{ color: "dodgerblue" }}></i>
-                <input className="wd-searchBar" placeholder = "Search for Your Professor........"/>
-                <a className="wd-switchToMajor" href="#"> I want to find a professor by major </a>
-            </div>
-
-
-
-        </>
-    )
+      <div className="wd-searchMain">
+        <i className="fas fa-cog fa-2x" style={{ color: "dodgerblue" }}></i>
+        <input
+          className="wd-searchBar"
+          placeholder="Search for Your Professor........"
+        />
+        <a className="wd-switchToMajor" href="#">
+          {" "}
+          I want to find a professor by major{" "}
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
