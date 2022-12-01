f81934805eedd586468f2d4a76f385cae09e0686
Alicefangxin <98005984+Alicefangxin@users.noreply.github.com>
2022-12-01 12:25:12 -0800

    Default page UI ongoing

--- src/components/unlogged-home/UnloggedHome.js
+++ src/components/unlogged-home/UnloggedHome.js
@@ -44,23 +44,23 @@ const UnloggedHome = () => {
                 </div>
                 <div className="mt-1">
                     <img
-                        className="rounded-5 me-1"
+                        className="rounded-5 ms-2 me-2"
                         src="https://pbs.twimg.com/media/ExQ97nJXMAM2AJH?format=jpg&name=medium"
                         width="350"
                      alt="none"/>
                     <img
-                        className="rounded-5 ms-1"
+                        className="rounded-5 ms-2"
                         src="https://i0.wp.com/news.northeastern.edu/wp-content/uploads/2021/10/husky-puppies-scaled.jpg?fit=2560%2C1704&ssl=1"
                         width="350"
                         alt="none"/>
                     <img
-                        className="rounded-5 ms-2"
+                        className="rounded-5 ms-3"
                         src="https://pbs.twimg.com/media/ExQ97a9WUAQNm3_?format=jpg&name=medium"
                         width="350"
                         alt="none"/>
                 </div>
             </div>
-            <div className="pt-2">
+            <div className="pt-2 mt-3">
                 <h2>Professors in Trends</h2>
             </div>
             <div className="p-3 mb-5 bg-info bg-opacity-10 border border-success border-start-0 border-end-0 rounded-end">
