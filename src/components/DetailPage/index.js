2a3e585bcb467e59e1f9866b8d9854ce4cd5d738
Alicefangxin <alicefangxingu@gmail.com>
2022-12-03 14:37:51 -0800

    corrected detail page with prof info from prof list

--- src/components/DetailPage/index.js
+++ src/components/DetailPage/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import "./index.css";
 import ReviewList from "./review-list";
 import ProfInfo from "./ProfInfo";
+import {useLocation} from "react-router";
 
-const DetailComponent = (info) => {
+const DetailComponent = () => {
+    const location = useLocation();
+    const info = location.state.info;
     return(
         <>
             <ProfInfo key={info._id} info={info}/>
